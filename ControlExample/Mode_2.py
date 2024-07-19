d4dd08c89d78d0c5f4f7ee96016229a5a37e5d4d
Hetcjj <2912531159@qq.com>
2024-07-19 14:20:46 +0800

    light control

---
+++ ControlExample/Mode_2.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import time
+
+# Define the IP address and port number of ESP32-C3
+url = "http://172.16.1.1/post"
+
+# Define the JSON data to be sent
+data_template = {
+    "id": 2,
+    "data": [
+        {"h": 32, "s": 255, "v": 255}
+    ]
+}
+
+# Send a POST request
+response = requests.post(url, json=data_template)
+
+# Print the response status code and result
+print(f"Status Code: {response.status_code}")
